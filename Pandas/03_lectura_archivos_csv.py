a3df7285e7be40cef667288d90d0da11ea1f449e
aastudillorazuri <aastudillorazuri@gmail.com>
2025-01-02 19:07:39 -0500

    Last update: 01/02/2025

--- Pandas/03_lectura_archivos_csv.py
+++ Pandas/03_lectura_archivos_csv.py
@@ -15,17 +15,20 @@ archivo_temperaturas = r'C:\Users\USER\Documents\Data\materiales-de-clase\Pandas
 # Leer CSV 'temperaturas_globales'
 df_temperaturas = pd.read_csv(archivo_temperaturas)
 
-# Mostrar los tipos de datos
-print(df_temperaturas.dtypes)
-
-# Leer el archivo CSV nuevamente con parseo de fechas (para asegurar que la columna 'Fecha' se interprete correctamente)
-df_temperaturas = pd.read_csv(archivo_temperaturas, parse_dates=['Fecha'])
-
-# Mostrar las primeras filas y los tipos de datos para verificar el parseo de fechas
+# Mostrar las primeras filas y los tipos de datos
 print(df_temperaturas.head())
 print(df_temperaturas.dtypes)
 
-# 
+'''
+Si estamos interesados solamente en las columnas 'Fecha' y 'Nueva York', podemos utilizar el argumento usecols para leer
+solo esas columnas:
+'''
+
+# Leer el archivo CSV seleccionando solo algunas columnas
+df_temperaturas_ny = pd.read_csv(archivo_temperaturas, usecols=['Fecha', 'Nueva York'])
+
+# Mostrar las primeras filas del DataFrame para verificar su contenido
+print(df_temperaturas_ny.head())
 
 ####################################################### Ejemplo 2 #######################################################
 
@@ -35,12 +38,17 @@ archivo_precipitaciones = r'C:\Users\USER\Documents\Data\materiales-de-clase\Pan
 # Leer CSV 'precipitaciones_globales'
 df_precipitaciones = pd.read_csv(archivo_precipitaciones)
 
-# Mostrar los tipos de datos
-print(df_precipitaciones.dtypes)
-
-# Leer el archivo CSV nuevamente con parseo de fechas (para asegurar que la columna 'Fecha' se interprete correctamente)
-df_precipitaciones = pd.read_csv(archivo_precipitaciones, parse_dates=['Fecha'])
-
-# Mostrar las primeras filas y los tipos de datos para verificar el parseo de fechas
+# Mostrar las primeras filas y los tipos de datos
 print(df_precipitaciones.head())
 print(df_precipitaciones.dtypes)
+
+'''
+Si estamos interesados solamente en las columnas 'Fecha' y 'París', podemos utilizar el argumento usecols para leer
+solo esas columnas:
+'''
+
+# Leer el archivo CSV seleccionando solo algunas columnas
+df_precipitaciones_ny = pd.read_csv(archivo_precipitaciones, usecols=['Fecha', 'París'])
+
+# Mostrar las primeras filas del DataFrame para verificar su contenido
+print(df_precipitaciones_ny.head())
