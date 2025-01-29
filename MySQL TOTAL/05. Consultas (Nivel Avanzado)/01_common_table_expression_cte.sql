5b991a38222401b90cf3189d151fbd608dddf4ac
aastudillorazuri <aastudillorazuri@gmail.com>
2025-01-29 11:43:58 -0500

    Last update: 01/29/2025

---
+++ MySQL TOTAL/05. Consultas (Nivel Avanzado)/01_common_table_expression_cte.sql
@@ -0,0 +1,7 @@
+/*
+Funcionan de manera similar a las subconsultas.
+Son consultas temporales que funcionan dentro de una consulta SQL.
+Las CTEs pueden pensarse como subconsultas que pueden nombrarse y luego referenciarlas
+dentro de la consulta principal.
+Las CTEs hacen que las querys sean más legibles y organizadas.
+Uno de los mayores beneficios de las CTEs 
